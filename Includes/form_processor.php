434f6c31357a7500f79cebcbd60811ce23655d5b
kiberu2023 <kiberusimonpeter095@gmail.com>
2023-11-27 10:32:01 -0800

    first commit

---
+++ Includes/form_processor.php
@@ -0,0 +1,5 @@
+mysqli_stmt_bind_param($stmt, "sssss",$fname, $)
+$fname=$_post["firstname"];
+$fname=$_post["firstname"];
+$fname=$_post["firstname"];
+$fname=$_post["firstname"];
--- add-guest.php
+++ add-guest.php
@@ -5,6 +5,27 @@
         <script src="bootstrap.min.css"></script>
     </head>
     <body>
-    <?php include "Includes/nav.php";?>  
+    <?php include "Includes/nav.php";?>
+    <div>
+        <form>
+            First name: <input type="text" name="First_name"/>
+            <br/>
+            Last name: <input type="text" name="Last_name"/>
+            <br/>
+            age:<input type="number" name="age"/>
+            <br/>
+            gender:<input type="text" name="gender"/>
+            <br/>
+            email:<input type="text" name="email"/>
+            <br/>
+            phone:<input type="text" name="phone"/>
+            <br/>
+            address:<input type="text" name="address"/>
+            <br/>
+            <input type="submit" value="Submit"/>
+
+
+        </form>
+    </div>
     </body>
 </html>
